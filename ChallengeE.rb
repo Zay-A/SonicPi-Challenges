20277a0cda04d9b6b300aea71542987642b3796d
Zay-A <zaydan_alim@student.davincischools.org>
2025-02-20 13:39:31 -0800

    ChallengeE.rb

---
+++ ChallengeE.rb
@@ -0,0 +1,53 @@
+
+# Challenge E
+use_bpm 180
+use_synth :chipbass
+
+# VARIABLES TO STORE SAMPLES
+vr =
+  
+
+
+# INTRO
+play :e4
+sleep 0.5
+play :e4
+sleep 1
+play :e4
+sleep 1
+play :c4
+sleep 0.5
+play :e4
+sleep 1
+play :g4
+sleep 2
+play :g3
+sleep 2
+
+
+# CUSTOM MARIO SAMPLES HERE
+sample "C:/Users/zaydan_alim/Downloads/mario_sounds/mario_sounds/mario_sounds/jump.wav"
+
+sleep 4
+
+sample "C:/Users/zaydan_alim/Downloads/mario_sounds/mario_sounds/mario_sounds/oneup.wav"
+
+sleep 4
+
+sample "C:/Users/zaydan_alim/Downloads/Aisudoツ - Close Near.wav"
+
+sleep 16
+
+sample "C:/Users/zaydan_alim/Downloads/Skrilla - Doot Doot (6 7) (Official Audio).wav"
+
+sleep 20
+
+
+# MUSHROOM SAMPLE HERE
+# https://www.youtube.com/watch?v=6G-k4zxou7Y
+
+sample "C:/Users/zaydan_alim/Downloads/Super Mario Bros. - Mushroom Sound Effect.wav"
+
+sleep 2
+
+stop
